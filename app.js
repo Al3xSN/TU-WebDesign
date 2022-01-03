4d7e5bc635fe5b67e9c4f1e2450fa4db308f1f22
Alex Nikolov <nikolovalex3@gmail.com>
2022-01-03 13:41:12 +0200

    refactor: style and funcs

--- app.js
+++ app.js
@@ -6,44 +6,17 @@ const formLastName = document.querySelector(".last-name");
 const formEmail = document.querySelector(".email");
 const formTxtArea = document.querySelector(".form-txtarea");
 
-
-const pureFuncs = {
-    replacingHeaderImage: (classList) => {
-        if (!header.classList.contains(classList)) {
-            header.classList.replace(header.classList, classList);
-            header.animate([{ opacity: '0.6' }, { opacity: '1' }], {
-                duration: 1200,
-            });
-        }
-    },
-    maniplatingClasses: (firstRemovebleButton, secondRemovebleButton, firstToggleButton) => {
-        pureFuncs.removeClass(firstRemovebleButton, 'active');
-        pureFuncs.removeClass(secondRemovebleButton, 'active');
-        pureFuncs.toggleClass(firstToggleButton, 'active');
-    },
-    removeClass: (button, className) => button.classList.remove(className),
-    toggleClass: (button, className) => button.classList.toggle(className)
-};
-
 const slideshowController = () => {
     btns.forEach(btn => {
         btn.addEventListener('click', () => {
-            if (btn.dataset.type === "s1") {
-                pureFuncs.replacingHeaderImage('first-header');
-                if (!btns[0].classList.contains('active')) {
-                    pureFuncs.maniplatingClasses(btns[1], btns[2], btns[0]);
-                }
-            } else if (btn.dataset.type === "s2") {
-                pureFuncs.replacingHeaderImage('second-header');
-                if (!btns[1].classList.contains('active')) {
-                    pureFuncs.maniplatingClasses(btns[0], btns[2], btns[1]);
-                }
-            } else {
-                pureFuncs.replacingHeaderImage('third-header');
-                if (!btns[2].classList.contains('active')) {
-                    pureFuncs.maniplatingClasses(btns[0], btns[1], btns[2]);
-                }
-            }
+            header.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(./resources/${btn.dataset.type}.jpg)`;
+            header.animate([{ opacity: '0.6' }, { opacity: '1' }], {
+                duration: 1200,
+            });
+
+            btns.forEach(button => {
+                button.dataset.type !== btn.dataset.type ? button.classList.remove('active') : button.classList.add('active');
+            });
         });
     });
 };
